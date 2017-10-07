f66ec539f7b2f40bd7524e1d54296bd228fb1438
DilkiIshara <dilkiishara922@gmail.com>
2017-10-07 16:35:19 +0530

    Organization Reg

---
+++ Projectformat/webapp/php/OrganizationReg.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Register Organization</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap-theme.css">
+    <link rel="stylesheet" href="../css/custom2.css">
+    <link rel="stylesheet" href="icon.css">
+    <link rel="stylesheet" href="awesome.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+    <div class="jamo">
+
+        <div class="container">
+            <h3>Helping Hand</h3>
+            <h1>JOIN with us</h1>
+
+
+
+            <div class="container" style="margin-top:50px">
+                <div class="row">
+                    <div class="col-md-offset-1 col-md-11 col-md-offset-1">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+
+                                <form action="" class="col-md-12" style="margin-top:0px padding-top: 0px;">
+
+                                    <div class="col-md-8">
+
+                                        <div class="form-group">
+                                            <label for="inputEmail" class="col-md-3 light">Email </label>
+                                            <div class="col-md-8">
+                                                <input type="text" name="email" class="form-control" id="email" placeholder="  example@abc.com">
+                                            </div>
+                                        </div>
+
+
+
+                                        <div class="form-group">
+                                            <label for="inputEmail" class="col-md-3 light">PassWord </label>
+                                            <div class="col-md-8">
+                                                <input type="password" name="" class="form-control" placeholder="  *************">
+                                            </div>
+                                        </div>
+
+
+
+
+
+                                        <div class="form-group">
+                                            <label for="select" class="col-md-3 light">Register as</label>
+                                            <div class="col-md-8">
+                                                <select class="select form-control" name="category" id="category">
+                                                                            <option>          Organization             </option>
+                                                                           <option>          Individual              </option>
+                                                                           <option>          Group            </option>
+                                                         </select>
+                                            </div>
+                                        </div>
+
+
+
+
+
+                                        <div class="form-group">
+                                            <label for="select" class="col-md-12 light2">Already have an account ? <a href="login.php" class="register"><u> Login</u> </a> here</label>
+                                        </div>
+
+
+                                    </div>
+
+                                    <div class="col-md-4">
+                                        <h2 style="color:yellow;margin-top:40px;text-align: center;font-size:50px;">WELCOME</h2>
+                                        <h5 style="color:yellow;">you are joining as new user</h5>
+                                    </div>
+
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="container" style="margin-top:0px; padding-top: 0px; padding-left:0px;">
+        <div class="row">
+            <form action="" style="margin-top:0px; padding-top: 0px; padding-left:0px;">
+
+                <div class="col-md-8">
+
+                    <h2>Personal Details</h2>
+
+                    <div class="form-group-2">
+                        <label for="inputEmail" class="col-md-3 lig">*Org.Name </label>
+                        <div>
+                            <input type="text" name="name" class="form-control-2" id="name" placeholder=" Organization registered name">
+                        </div>
+                    </div>
+
+
+
+                    <div class="form-group-2">
+                        <label class="col-md-3 lig">Official Web site </label>
+                        <div>
+                            <input type="text" name="" class="form-control-2" placeholder=" www.abc.lk">
+                        </div>
+                    </div>
+
+
+
+                    <div class="form-group-2">
+                        <label class="col-md-3 lig">*Contact no </label>
+                        <div>
+                            <input type="text" name="" class="form-control-2" placeholder=" Contact number">
+                        </div>
+                    </div>
+
+
+                    <div class="form-group-2">
+                        <label class="col-md-3 lig">*Address </label>
+                        <div>
+                            <input type="text" name="" class="form-control-2" placeholder="Official address"> <br>
+                            <input type="text" name="" class="form-control-2" style="margin-top:20px;" placeholder="Postal code">
+
+                        </div>
+                    </div>
+
+
+
+
+
+
+
+                    <div class="form-group">
+
+                    </div>
+                    <button class="btn-noveri">REGISTER(skip verification)</button>
+                    <button class="btn-veri">Continue to verification ></button>
+
+                </div>
+
+                <div class="col-md-4">
+
+
+                    <div col-md-3> <img src="../images/images.jpg" class="img"><br>
+                        <p>Org. Logo <button class="btn-browse" value="Browse">Browse </button></p>
+                    </div>
+
+                </div>
+
+
+
+
+            </form>
+        </div>
+
+    </div>
+
+
+
+
+
+
+    <!--Start of the footer-->
+
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+
+
+                <div class="col-md-2">
+                    <p class="footer">Helping Hand </p>
+                </div>
+
+
+                <div class="col-md-3" style="padding-top:20px">
+                    <a href="#"><i class="glyphicon glyphicon-envelope fa-2x"></i></a>
+                    <a href="Helpinghand@gmail.com">Helpinghand@gmail.com</a><br><br>
+                    <a href="sitemap.html">site map</a>
+
+                </div>
+
+
+                <div class="col-md-3" style="padding-top:20px">
+                    <a href="#"><i class="fa fa-facebook fa-2x"></i></a>
+                    <a href="https://www.facebook.com/x">https://www.facebook.com</a>
+                </div>
+
+
+
+
+
+                <div class="github" style="padding-top:20px">
+                    <a href="#"><i class="fa fa-github fa-2x"></i></a> - <a href="https://github.com/KasunMadusanka/testing">https://github.com/KasunMadusanka/testing</a>
+                </div>
+
+
+            </div>
+
+            <div class="bottom-footer">
+                <div class="col-md-6"> </div>
+
+            </div>
+
+
+        </div>
+
+    </footer>
+
+    <!--End Of the Footer-->
+
+</body>
+
+
+
+
+
+</html>
