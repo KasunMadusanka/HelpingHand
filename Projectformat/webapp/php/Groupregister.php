8d6ee9ef796dd59e8173940061e45575adfd0cb9
shashinichathurika <shashinichathurika30@gmail.com>
2017-10-12 13:24:38 +0530

    Group register

---
+++ Projectformat/webapp/php/Groupregister.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>Transparent Login Form</title>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap-theme.css">
+    <link rel="stylesheet" href="../css/custom3.css">
+    <link rel="stylesheet" href="icon.css">
+    <link rel="stylesheet" href="awesome.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> </head>
+
+<body style="background:beige;">
+    <div class="jamo">
+        <div class="container">
+            <h3>Helping Hand</h3>
+            <h1>JOIN with us</h1>
+            <div class="container" style="margin-top:50px">
+                <div class="row">
+                    <div class="col-md-offset-1 col-md-11 col-md-offset-1">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <form action="" class="col-md-12" style="margin-top:0px padding-top: 0px;">
+                                    <div class="col-md-8">
+                                        <div class="form-group">
+                                            <label for="inputEmail" class="col-md-3 light">Email </label>
+                                            <div class="col-md-8">
+                                                <input type="text" name="email" class="form-control" id="email" placeholder="  Enter Email"> </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="inputEmail" class="col-md-3 light">PassWord </label>
+                                            <div class="col-md-8">
+                                                <input type="password" name="" class="form-control" placeholder="*************"> </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="select" class="col-md-3 light">Login as</label>
+                                            <div class="col-md-8">
+                                                <select class="select form-control" name="category" id="category">
+                                                    <option> Donor </option>
+                                                    <option> Pauper </option>
+                                                    <option> Verfier </option>
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="select" class="col-md-8 light">Already have an account ? <a href="login.html" class="register"><u> Login</u> </a> here</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <h2 style="color:yellow;margin-top:40px;text-align: center;font-size:50px;">WELCOME</h2>
+                                        <h5 style="color:yellow;">you are joining as new user</h5> </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container" style="background:beige;margin-top:0px; padding-top: 0px; padding-left:0px;">
+        <div class="row">
+            <form action="" style="margin-top:0px; padding-top: 0px; padding-left:0px;">
+                <div class="col-md-8">
+                    <div class="form-group-2">
+                        <label for="inputEmail" class="col-md-3 lig">*Group Name </label>
+                        <div>
+                            <input type="text" name="name" class="form-control-2" id="name" placeholder="Group name"> </div>
+                    </div>
+                    <div class="form-group-2">
+                        <div class="col-mg-2">
+                            <label class="col-md-4 lig">*Contact No.s </label>
+                        </div>
+                        <div class="col-sm-7" style="padding-left:-274%;margin-left:-14%;width:220px;">
+                            <div class="panel3" style="padding-left:0px;margin-left:0px; height:30px;background-color:#5b9bd5;color:white;">
+                                <div class="col-sm-6">
+                                    <input type="text" name="" class="form-inputpa" id="" style="color:white;" placeholder="0775580114"> </div>
+                                <div class="col-sm-6">
+                                    <input type="image" class="contact-delete" src="../images/delete.jpeg" alt=""> </div>
+                            </div>
+                        </div>
+                    </div>
+                    <input type="text" name="" class="form-control-2" style="margin-top:-75px;width:125px;" placeholder="Contact No">
+                    <br>
+                    <div class="form-group-2">
+                        <label class="col-md-3 lig">*Group Address </label>
+                        <div>
+                            <input type="text" name="" class="form-control-2" placeholder="Group Address">
+                            <br>
+                            <input type="text" name="" class="form-control-2" style="margin-top:20px;" placeholder="Postel Code"> </div>
+                    </div>
+                    <h2>Group Member Details</h2>
+                    <div class="panel2 panel-default">
+                        <div class="panel-body">
+                            <div class="col-sm-1"> <img src="../images/img.jpg" alt="" class="profi" style="padding-left=0px;margin-left=0px;"></div>
+                            <input type="text" name="email" class="form-inputpa" id="email" placeholder="K.D.Kasun Madusanka">
+                            <input type="text" name="email" class="form-inputpa" id="email" placeholder="910xxxxxxV">
+                            <button class="btn-verify1"><img style="width:30px; height:30px;padding-right:0px;border-radius: 80px;;" src="../images/bin.png"></button>
+                        </div>
+                    </div>
+                    <div class="col-lg-9">
+                        <div class="form-group-2">
+                            <label for="inputEmail" class="col-md-3 lig">*Name </label>
+                            <div>
+                                <input type="text" name="name" class="form-control-2" id="name" placeholder="Member name"> </div>
+                        </div>
+                        <div class="form-group-2">
+                            <label for="inputEmail" class="col-md-3 lig">*NIC No</label>
+                            <div>
+                                <input type="text" name="name" class="form-control-2" id="name" placeholder="xxxxxxxxxV"> </div>
+                        </div>
+                    </div>
+                    <div class="form-group-2"> </div> <img class="profile" src="../images/group.png">
+                    <button class="btn-verify3">+</button>
+                    <button class="btn-noveri">REGISTER(skip verification)</button>
+                    <button class="btn-veri">Continue to verification ></button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!--Start of the footer-->
+    <footer class="site-footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2">
+                    <p class="footer">Helping Hand </p>
+                </div>
+                <div class="col-md-3" style="padding-top:20px"> <a href="#"><i class="glyphicon glyphicon-envelope fa-2x"></i></a> <a href="Helpinghand@gmail.com">Helpinghand@gmail.com</a>
+                    <br>
+                    <br> <a href="sitemap.html">site map</a> </div>
+                <div class="col-md-3" style="padding-top:20px"> <a href="#"><i class="fa fa-facebook fa-2x"></i></a> <a href="https://www.facebook.com/x">https://www.facebook.com</a> </div>
+                <div class="github" style="padding-top:20px"> <a href="#"><i class="fa fa-github fa-2x"></i></a> - <a href="https://github.com/KasunMadusanka/testing">https://github.com/KasunMadusanka/testing</a> </div>
+            </div>
+            <div class="bottom-footer">
+                <div class="col-md-6"> </div>
+            </div>
+        </div>
+    </footer>
+    <!--End Of the Footer-->
+</body>
+
+</html>
